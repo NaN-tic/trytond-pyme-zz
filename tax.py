26be2b66007b01d81e41138fee7cbfd296b0d7df
Jordi Esteve <jesteve@zikzakmedia.com>
2015-03-17 21:49:40 +0100

    Allow add tax lines in move lines. Add missing file

---
+++ tax.py
@@ -0,0 +1,21 @@
+# This file is part pyme module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['TaxLine']
+__metaclass__ = PoolMeta
+
+
+class TaxLine():
+    __name__ = 'account.tax.line'
+
+    def on_change_tax(self):
+        values = super(TaxLine, self).on_change_tax()
+        del values['code']
+        return values
+
+    @fields.depends('move_line')
+    def on_change_with_company(self, name=None):
+        return super(TaxLine, self).on_change_with_company(name)
