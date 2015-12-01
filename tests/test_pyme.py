2ecf53ea351212d8024535df953ae98d012bf94c
resteve <resteve@zikzakmedia.com>
2015-12-01 11:17:47 +0100

    Tests

--- tests/test_pyme.py
+++ tests/test_pyme.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part pyme module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the pyme module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PymeTestCase(unittest.TestCase):
-    'Test PyME module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('pyme')
-
-    def test0005views(self):
-        'Test views'
-        test_view('pyme')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class PymeTestCase(ModuleTestCase):
+    'Test Pyme module'
+    module = 'pyme'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PymeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
