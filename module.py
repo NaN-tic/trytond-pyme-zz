d2ec97ad1136dea3d138f1422ca91b54cccbb39d
raimonesteve <raimonesteve@gmail.com>
2020-03-21 17:00:49 +0100

    Upgrade last changes 5.x

--- module.py
+++ module.py
@@ -3,6 +3,8 @@
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 from trytond.transaction import Transaction
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 __all__ = ['Module']
 
@@ -11,23 +13,13 @@ class Module(metaclass=PoolMeta):
     __name__ = "ir.module"
 
     @classmethod
-    def __setup__(cls):
-        super(Module, cls).__setup__()
-        cls._error_messages.update({
-                'activation_error': 'You cannot modify the state of a module '
-                    'since desktop client because this is a balanced server.\n'
-                    'Please, contact with your TIC provider in order to do it '
-                    'for you. Thank you.',
-                })
-
-    @classmethod
     def activate(cls, modules):
         if Transaction().user != 0:
-            cls.raise_user_error('activation_error')
+            raise UserError(gettext('pyme.msg_activation_error'))
         super(Module, cls).activate(modules)
 
     @classmethod
     def deactivate(cls, modules):
         if Transaction().user != 0:
-            cls.raise_user_error('activation_error')
+            raise UserError(gettext('pyme.msg_activation_error'))
         super(Module, cls).deactivate(modules)
